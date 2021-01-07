a59f1e57491d08b9b9ce7a5d6980681693726fbe
Alessandro Silva <alessandro.rds03@gmail.com>
2021-01-07 15:41:44 -0400

    adding migration access tokens and controllers forgot and reset of password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 // import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 // import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/accesses/infra/http/routes/sessions.routes';
-// import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import passwordRouter from '@modules/accesses/infra/http/routes/password.routes';
 // import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import accessesRouter from '@modules/accesses/infra/http/routes/accesses.routes';
 
@@ -14,7 +14,7 @@ routes.use('/accesses', accessesRouter);
 // routes.use('/providers', providersRouter);
 // routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
-// routes.use('/password', passwordRouter);
+routes.use('/password', passwordRouter);
 // routes.use('/profile', profileRouter);
 
 export default routes;
---
+++ src/shared/infra/typeorm/migrations/1610046240231-CreateAcessTokens.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAcessTokens1610046240231
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'access_tokens',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'token',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'access_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'TokenAccess',
+            referencedTableName: 'accesses',
+            referencedColumnNames: ['id'],
+            columnNames: ['access_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('access_tokens');
+  }
+}
